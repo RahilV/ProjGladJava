9dd0121f365f73035b4d61e4b6b59f24f5a35ec0
RahilV <rahil.v@somaiya.edu>
2021-08-15 13:56:59 +0530

    products purchased buy functionality - saurav

--- finance/src/main/java/com/lti/gladiator/finance/beans/ProductsPurchased.java
+++ finance/src/main/java/com/lti/gladiator/finance/beans/ProductsPurchased.java
@@ -13,112 +13,113 @@ import javax.persistence.Table;
 @Entity
 @Table(name="PRODUCTS_PURCHASED")
 public class ProductsPurchased {
-	
-	
-	@Id
-	@Column(name="PRODUCT_PURCHASED_ID")
-	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator = "HB_PP_SEQ")
+        
+    @Id
+    @Column(name="PRODUCT_PURCHASED_ID")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE,generator = "HB_PP_SEQ")
     @SequenceGenerator(name="HB_PP_SEQ", sequenceName="PP_Seq" ,allocationSize=1)
-	private int productPurchasedId;
-	
-	@Column(name="USER_ID")
-	private int userId;
-	
-	@OneToOne()
-	@JoinColumn(name="PRODUCT_ID")
-	private Products product;
-	
-	@Column(name="EMI_DURATION")
-	private String emiDuration;
-	
-	@Column(name="AMOUNT_BILLABLE")
-	private int amountBillable;
-	
-	@Column(name="AMOUNT_PAYED")
-	private int amountPayed;
-	
-	@Column(name="TRANSACTION_ID")
-	private int transactionId;
+    private int productPurchasedId;
+    
+    @Column(name="USER_ID")
+    private int userId;
+    
+    @OneToOne()
+    @JoinColumn(name="PRODUCT_ID")
+    private Products productId;
+    
+    @Column(name="AMOUNT_BILLABLE")
+    private int amountBillable;
+    
+    @Column(name="AMOUNT_PAYED")
+    private int amountPayed;
+    
+    @Column(name="TRANSACTION_ID")
+    private int transactionId;
+    
+    @Column(name="EMI_PERIOD")
+    private int emiPeriod;
 
-	public ProductsPurchased(int productPurchasedId, int userId, Products product, String emiDuration, int amountBillable,
-			int amountPayed, int transactionId) {
-		super();
-		this.productPurchasedId = productPurchasedId;
-		this.userId = userId;
-		this.product = product;
-		this.emiDuration = emiDuration;
-		this.amountBillable = amountBillable;
-		this.amountPayed = amountPayed;
-		this.transactionId = transactionId;
-		
-		
-	}
+    public ProductsPurchased(int productPurchasedId, int userId, Products productId, int amountBillable,
+            int amountPayed, int transactionId, int emiPeriod) {
+        super();
+        this.productPurchasedId = productPurchasedId;
+        this.userId = userId;
+        this.productId = productId;
+        this.amountBillable = amountBillable;
+        this.amountPayed = amountPayed;
+        this.transactionId = transactionId;
+        this.emiPeriod = emiPeriod;
+    }
 
-	public ProductsPurchased() {
-		super();
-	}
+    public ProductsPurchased() {
+        super();
+    }
 
-	public int getProductPurchasedId() {
-		return productPurchasedId;
-	}
+    public int getProductPurchasedId() {
+        return productPurchasedId;
+    }
 
-	public void setProductPurchasedId(int productPurchasedId) {
-		this.productPurchasedId = productPurchasedId;
-	}
+    public void setProductPurchasedId(int productPurchasedId) {
+        this.productPurchasedId = productPurchasedId;
+    }
 
-	public int getUserId() {
-		return userId;
-	}
+    public int getUserId() {
+        return userId;
+    }
 
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
-	public Products getProduct() {
-		return product;
-	}
+    public Products getProductId() {
+        return productId;
+    }
 
-	public void setProduct(Products product) {
-		this.product = product;
-	}
+    public void setProductId(Products productId) {
+        this.productId = productId;
+    }
 
-	public String getEmiDuration() {
-		return emiDuration;
-	}
+    public int getAmountBillable() {
+        return amountBillable;
+    }
 
-	public void setEmiDuration(String emiDuration) {
-		this.emiDuration = emiDuration;
-	}
+    public void setAmountBillable(int amountBillable) {
+        this.amountBillable = amountBillable;
+    }
 
-	public int getAmountBillable() {
-		return amountBillable;
-	}
+    public int getAmountPayed() {
+        return amountPayed;
+    }
 
-	public void setAmountBillable(int amountBillable) {
-		this.amountBillable = amountBillable;
-	}
+    public void setAmountPayed(int amountPayed) {
+        this.amountPayed = amountPayed;
+    }
 
-	public int getAmountPayed() {
-		return amountPayed;
-	}
+    public int getTransactionId() {
+        return transactionId;
+    }
 
-	public void setAmountPayed(int amountPayed) {
-		this.amountPayed = amountPayed;
-	}
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
 
-	public int getTransactionId() {
-		return transactionId;
-	}
+    public int getEmiPeriod() {
+        return emiPeriod;
+    }
 
-	public void setTransactionId(int transactionId) {
-		this.transactionId = transactionId;
-	}
+    public void setEmiPeriod(int emiPeriod) {
+        this.emiPeriod = emiPeriod;
+    }
 
-	@Override
-	public String toString() {
-		return "ProductsPurchased [productPurchasedId=" + productPurchasedId + ", userId=" + userId + ", product="
-				+ product + ", emiDuration=" + emiDuration + ", amountBillable=" + amountBillable + ", amountPayed="
-				+ amountPayed + ", transactionId=" + transactionId + "]";
-	}
-	
+    @Override
+    public String toString() {
+        return "ProductsPurchased [productPurchasedId=" + productPurchasedId + ", userId=" + userId + ", productId="
+                + productId + ", amountBillable=" + amountBillable + ", amountPayed=" + amountPayed + ", transactionId="
+                + transactionId + ", emiPeriod=" + emiPeriod + "]";
+    }
+
+
+    
+    
+    
 }
